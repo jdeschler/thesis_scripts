a057a505ba4795c3e80ead513850e447955e1813
Jack Deschler <deschler.jack@gmail.com>
2019-01-20 17:11:53 -0500

    import itertools

--- party_model_nc.py
+++ party_model_nc.py
@@ -8,8 +8,8 @@ import argparse
 import numpy as np
 import pandas as pd
 import scipy as sp
-import sys
 import gc
+import itertools
 #from joblib import dump, load
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.ensemble import RandomForestClassifier
